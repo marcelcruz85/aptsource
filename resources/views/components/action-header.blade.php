133d065d9316d6f9eb4d82ae5d7705d65eaaa1d0
Marcel Cruz <mcruz@consumerlaw.com>
2018-03-27 17:36:52 -0500

    listing grid

---
+++ resources/views/components/action-header.blade.php
@@ -0,0 +1,30 @@
+<div class="action-header">
+        <div class="container">
+            <div class="action-header__item action-header__item--search">
+                <form>
+                    <input class="hidden-xs" type="text" placeholder="Search by neighborhood, city, zip or address..."><!-- For desktop -->
+                    <input class="visible-xs" type="text" placeholder="Search..."><!-- For mobile -->
+                </form>
+            </div>
+
+            <div class="action-header__item action-header__views hidden-xs">
+                <a href="listings-grid.html" class="zmdi zmdi-apps active"></a>
+                <a href="listings-list.html" class="zmdi zmdi-view-list"></a>
+                <a href="listings-map.html" class="zmdi zmdi-map"></a>
+            </div>
+
+            <div class="action-header__item action-header__item--sort hidden-xs">
+                <span class="action-header__small">Sort by :</span>
+
+                <select class="select2">
+                    <option>Featured listings</option>
+                    <option>Newest to oldest</option>
+                    <option>Oldest to Newest</option>
+                    <option>Price hight to low</option>
+                    <option>Price low to high</option>
+                    <option>Newest to Oldest</option>
+                    <option>No. of photos</option>
+                </select>
+            </div>
+        </div>
+    </div>
