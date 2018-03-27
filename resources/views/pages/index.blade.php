bddf9288032d07aff68685a8a560c71974f97419
Marcel Cruz <mcruz@consumerlaw.com>
2018-03-27 17:19:03 -0500

    layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -380,3 +380,75 @@
         </div>
     </div>
 @endsection
+
+@section ('submit-ticker')
+<p>Are you looking to sell your valuable property or rent out? Roost is your destination!</p>
+
+<a href="submit-property.html">Submit your property</a>
+@endsection
+
+@section ('info-box')
+<div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="info-box__item">
+                    <i class="zmdi zmdi-trending-up info-box__icon"></i>
+    
+                    <h3>Sed posuere consectetur</h3>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel
+                        augue laoreet rutrum faucibus dolor auctor.</p>
+                </div>
+            </div>
+    
+            <div class="col-sm-4">
+                <div class="info-box__item">
+                    <i class="zmdi zmdi-search info-box__icon"></i>
+    
+                    <h3>Donec ullamcorper nulla</h3>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel
+                        augue laoreet rutrum faucibus dolor auctor.</p>
+                </div>
+            </div>
+    
+            <div class="col-sm-4">
+                <div class="info-box__item">
+                    <i class="zmdi zmdi-globe info-box__icon"></i>
+    
+                    <h3>Integer posuere erat</h3>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel
+                        augue laoreet rutrum faucibus dolor auctor.</p>
+                </div>
+            </div>
+    
+            <div class="col-sm-4">
+                <div class="info-box__item">
+                    <i class="zmdi zmdi-flower info-box__icon"></i>
+    
+                    <h3>Nulla vitae elit</h3>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel
+                        augue laoreet rutrum faucibus dolor auctor.</p>
+                </div>
+            </div>
+    
+            <div class="col-sm-4">
+                <div class="info-box__item">
+                    <i class="zmdi zmdi-pin info-box__icon"></i>
+    
+                    <h3>Libero pharetra augue</h3>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel
+                        augue laoreet rutrum faucibus dolor auctor.</p>
+                </div>
+            </div>
+    
+            <div class="col-sm-4">
+                <div class="info-box__item">
+                    <i class="zmdi zmdi-sun info-box__icon"></i>
+    
+                    <h3>Aenean lacinia bibendum</h3>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Vivamus sagittis lacus vel
+                        augue laoreet rutrum faucibus dolor auctor.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
