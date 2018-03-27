34871be5e9a5ad4810c8d36d43bca9e799238976
Marcel Cruz <mcruz@consumerlaw.com>
2018-03-27 18:03:30 -0500

    app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'The Appartment Source'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
