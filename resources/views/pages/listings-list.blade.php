addb27eb8d5fac9a9a5b3cf8fa6ac3a756880f1f
Marcel Cruz <mcruz@consumerlaw.com>
2018-03-29 14:10:35 -0500

    picture

--- resources/views/pages/listings-list.blade.php
+++ resources/views/pages/listings-list.blade.php
@@ -47,7 +47,7 @@
     <div class="row">
         <div class="col-sm-8 listings-list">
             
-                {{ dd($listings->Photos)}}
+                {{ dd($listings->Photos->Photo)}}
             @foreach ($listings as $listing)
 
             <div class="listings-grid__item">
