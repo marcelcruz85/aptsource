7ba1fc34b551a0e835a2f9c740d980607c72650d
Marcel Cruz <mcruz@consumerlaw.com>
2018-03-29 18:23:19 -0500

    testing

--- resources/views/pages/listings-list.blade.php
+++ resources/views/pages/listings-list.blade.php
@@ -50,7 +50,7 @@
             @foreach ($listings as $listing)
         
             @foreach ($listing as $k => $v)
-                {{ dd($v) }}
+                <? print_r($v) ?>
             @endforeach
             <div class="listings-grid__item">
                 <a href="listing-detail.html" class="media">
