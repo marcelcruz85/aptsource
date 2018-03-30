99de5ae7cebed3959fef3bcb81bcffcbb187c446
Marcel Cruz <mcruz@consumerlaw.com>
2018-03-30 16:45:11 -0500

    testing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -13,7 +13,7 @@ class ListingController extends Controller
         $res = $client->request('POST', 'https://www.yougotlistings.com/api/rentals/search.php?key=bVrLNhG2U1aFCKuix97RdsQyIfEnXPpl8jcSvzZO&detail_level=2');
 
         $xml = $res->getBody();
-        $xml = simplexml_load_string($xml);
+        $xml = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
         $json = json_encode($xml);
         $response = json_decode($json, FALSE);
     
