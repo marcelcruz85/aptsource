753a594ee8136c34af36524688a70eccb9865f87
Marcel Cruz <mcruz@consumerlaw.com>
2018-03-30 12:09:45 -0500

    testing

--- resources/views/pages/listings-list.blade.php
+++ resources/views/pages/listings-list.blade.php
@@ -42,7 +42,7 @@
 
     <div class="row">
         <div class="col-sm-8 listings-list">
-
+            {{ dd($listings)}}
             @foreach ($listings as $listing)
             
             @if (is_array($listing) and array_key_exists('Title', $listing) )
