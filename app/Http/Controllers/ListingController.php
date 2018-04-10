a2f8e0efae2ee07f869418388f1667b0f27269ff
Marcel Cruz <mcruz@consumerlaw.com>
2018-04-10 12:05:07 -0500

    query path

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -42,7 +42,7 @@ class ListingController extends Controller
             'sort' => $sort,
             'pages' =>  $pages,
             'pageIndex' =>  $page,
-            'urlParameters' => $request->getQueryString(),
+            'urlParameters' => $request->query(),
             'location' => $location,
             'listings' => $response['Listings']['Listing'],
         ]);
