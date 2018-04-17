e106ffede13cb5b0f0ae817afd4e4ea0a104a6ba
Marcel Cruz <mcruz@consumerlaw.com>
2018-04-17 15:24:12 -0500

    w4bv456ysd

--- resources/views/pages/listings-maps.blade.php
+++ resources/views/pages/listings-maps.blade.php
@@ -57,47 +57,50 @@
                 data: {
                     "location": "60601"
                 },
-                success: function () {
+                success: function (data) {
+
+                    console.log(data);
+                    
                     var uluru = {
-            lat: -25.363,
-            lng: 131.044
-        };
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 4,
-            center: uluru
-        });
+                        lat: -25.363,
+                        lng: 131.044
+                    };
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                        zoom: 4,
+                        center: uluru
+                    });
 
 
-        var contentString = '<div id="content">' +
-            '<div id="siteNotice">' +
-            '</div>' +
-            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-            '<div id="bodyContent">' +
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            'sandstone rock formation in the southern part of the ' +
-            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-            'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-            'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-            'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-            'Aboriginal people of the area. It has many springs, waterholes, ' +
-            'rock caves and ancient paintings. Uluru is listed as a World ' +
-            'Heritage Site.</p>' +
-            '</div>' +
-            '</div>';
+                    var contentString = '<div id="content">' +
+                        '<div id="siteNotice">' +
+                        '</div>' +
+                        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+                        '<div id="bodyContent">' +
+                        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                        'sandstone rock formation in the southern part of the ' +
+                        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+                        'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+                        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+                        'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+                        'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+                        'Aboriginal people of the area. It has many springs, waterholes, ' +
+                        'rock caves and ancient paintings. Uluru is listed as a World ' +
+                        'Heritage Site.</p>' +
+                        '</div>' +
+                        '</div>';
 
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
+                    var infowindow = new google.maps.InfoWindow({
+                        content: contentString
+                    });
 
-        var marker = new google.maps.Marker({
-            position: uluru,
-            map: map,
-            title: 'Uluru (Ayers Rock)'
-        });
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
+                    var marker = new google.maps.Marker({
+                        position: uluru,
+                        map: map,
+                        title: 'Uluru (Ayers Rock)'
+                    });
+                    marker.addListener('click', function () {
+                        infowindow.open(map, marker);
+                    });
                 },
                 error: function () {
                     console.log('API request fail');
