1a8697732e323e7fcf98adc61a354421082f6921
Marcel Cruz <mcruz@consumerlaw.com>
2018-04-17 16:48:19 -0500

    wetq34sv

--- resources/views/pages/listings-maps.blade.php
+++ resources/views/pages/listings-maps.blade.php
@@ -55,7 +55,7 @@
                 type: "GET",
                 url: url,
                 data: {
-                    "location": "60601"
+                    "location": ""
                 },
                 success: function (data) {
 
