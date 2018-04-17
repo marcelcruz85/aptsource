a2a05ecbd4a02a84c24c07df5b2dabcee9311f53
Marcel Cruz <mcruz@consumerlaw.com>
2018-04-17 16:18:13 -0500

    erdasfq34

--- resources/views/pages/listings-maps.blade.php
+++ resources/views/pages/listings-maps.blade.php
@@ -63,10 +63,6 @@
                     var listings = data;
 
 
-                    var uluru = {
-                        lat: -25.363,
-                        lng: 131.044
-                    };
                     var map = new google.maps.Map(document.getElementById('map'), {
                         zoom: 4,
                         center: uluru
@@ -96,6 +92,10 @@
                     });
 
                     for (i = 0; i < listings.length; i++) {  
+                        var uluru = {
+                            lat: -25.363,
+                            lng: 131.044
+                        };
                         var marker = new google.maps.Marker({
                             position: {lat: -25.363, lng: 131.044},
                             map: map,
