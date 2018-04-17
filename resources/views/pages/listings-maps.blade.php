6efb0264c107ac41ef075d4d53038f9562b28721
Marcel Cruz <mcruz@consumerlaw.com>
2018-04-17 13:25:56 -0500

    qwrq3re

--- resources/views/pages/listings-maps.blade.php
+++ resources/views/pages/listings-maps.blade.php
@@ -1,14 +1,10 @@
-@extends ('layout') 
-
-@section ('header')
+@extends ('layout') @section ('header')
 
 <header id="header" class="header--minimal">
     @include ('components.top-header') @include ('components.main-header')
 </header>
 
-@endsection 
-
-@section ('action-header')
+@endsection @section ('action-header')
 
 <div class="action-header">
     <div class="container">
@@ -19,7 +15,7 @@
                 <input class="visible-xs autocomplete" type="text" placeholder="Search...">
                 <!-- For mobile -->
                 <div class="search__type">
-                        <button class="btn btn-primary not-show">Search</button>
+                    <button class="btn btn-primary not-show">Search</button>
                 </div>
             </form>
         </div>
@@ -48,69 +44,69 @@
 
 @endsection 
 
-@section ('content') 
-    <div id="map"></div>
-    <div id="properties"></div>
-    <script>
-$(document).ready(function () {
-    $.ajax({
-        type: "GET",
-        url: 'http://aptsource.dotgital.com/rentals/api/search',
-        data: {
-            "Zip": "60601"
+@section ('content')
+<div id="map"></div>
+<div id="properties"></div>
+<script>
+    function initMap() {
+
+        $.ajax({
+            type: "GET",
+            url: 'http://aptsource.dotgital.com/rentals/api/search',
+            data: {
+                "Zip": "60601"
             },
-        success: function(data){
-            console.log(data);
-            function initMap() {
-                var uluru = {lat: -25.363, lng: 131.044};
-                var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: uluru
-                });
+            success: function (data) {
+                console.log(data);
+            },
+            error: function () {
+                console.log('API request fail');
+            },
+        });
+
+        var uluru = {
+            lat: -25.363,
+            lng: 131.044
+        };
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
 
 
-                var contentString = '<div id="content">'+
-                    '<div id="siteNotice">'+
-                    '</div>'+
-                    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-                    '<div id="bodyContent">'+
-                    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-                    'sandstone rock formation in the southern part of the '+
-                    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-                    'south west of the nearest large town, Alice Springs; 450&#160;km '+
-                    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-                    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-                    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-                    'Aboriginal people of the area. It has many springs, waterholes, '+
-                    'rock caves and ancient paintings. Uluru is listed as a World '+
-                    'Heritage Site.</p>'+
-                    '</div>'+
-                    '</div>';
+        var contentString = '<div id="content">' +
+            '<div id="siteNotice">' +
+            '</div>' +
+            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+            '<div id="bodyContent">' +
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'sandstone rock formation in the southern part of the ' +
+            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+            'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+            'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+            'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+            'Aboriginal people of the area. It has many springs, waterholes, ' +
+            'rock caves and ancient paintings. Uluru is listed as a World ' +
+            'Heritage Site.</p>' +
+            '</div>' +
+            '</div>';
 
-                var infowindow = new google.maps.InfoWindow({
-                content: contentString
-                });
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
 
-                var marker = new google.maps.Marker({
-                position: uluru,
-                map: map,
-                title: 'Uluru (Ayers Rock)'
-                });
-                marker.addListener('click', function() {
-                infowindow.open(map, marker);
-                });
-            }
-            <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDbHsbSThuJHEQpfVqp91y3CRS5KiXxS-4&callback=initMap">
-    </script>
-        },
-        error: function(){
-            console.log('API request fail');
-        },
-    });
-});
-      
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map,
+            title: 'Uluru (Ayers Rock)'
+        });
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+    }
 </script>
-
-    
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDbHsbSThuJHEQpfVqp91y3CRS5KiXxS-4&callback=initMap">
+</script>
 
 @endsection
