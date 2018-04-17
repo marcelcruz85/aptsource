245400ce3e1df9741dde661c03ee8c58e623f476
Marcel Cruz <mcruz@consumerlaw.com>
2018-04-17 11:12:59 -0500

    jquery  javascript

--- resources/views/pages/listings-maps.blade.php
+++ resources/views/pages/listings-maps.blade.php
@@ -59,7 +59,7 @@
           center: uluru
         });
 
-        var listing = $("#properties").text();
+        var listing = jQuery("#properties").text();
         console.log(listing);
         var contentString = '<div id="content">'+
             '<div id="siteNotice">'+
