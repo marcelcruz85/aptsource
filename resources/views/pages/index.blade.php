e2b7197daed73aca91ad12c10316bf0dc390ef4d
Marcel Cruz <mcruz@consumerlaw.com>
2018-04-18 13:52:47 -0500

    sdfq34s

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -67,7 +67,7 @@
 <section class="section submit-ticker"> 
 <p>Are you looking to sell your valuable property or rent out? Roost is your destination!</p>
 
-<a href="submit-property.html">Submit your property</a>
+<a href="/submit">Submit your property</a>
 </section>
 
 
