79b3d2ec8bd3f1ce8d7a81f4c2afcd8bb1386b46
Marcel Cruz <mcruz@consumerlaw.com>
2018-04-18 16:08:00 -0500

    wsef2351234

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,11 +3,8 @@
 @section ('head-scripts')
 
   <!-- ... -->
-  <script type="text/javascript" src="/vendors/bower_components/jquery/jquery.min.js"></script>
   <script type="text/javascript" src="/vendors/bower_components/moment/min/moment.min.js"></script>
-  <script type="text/javascript" src="/vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="/vendors/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-  <link rel="stylesheet" href="/vendors/bower_components/bootstrap/dist/css/bootstrap.min.css" />
   <link rel="stylesheet" href="/vendors/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
 
 @endsection
