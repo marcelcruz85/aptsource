faa8fe2fdc44241e59f5128e57dc2e1852a6b0c6
Marcel Cruz <mcruz@consumerlaw.com>
2018-04-18 17:57:39 -0500

    123123

--- public/js/demo/demo.js
+++ public/js/demo/demo.js
@@ -61,7 +61,7 @@ if ($('#available-date-range')[0]) {
             max: timestamp('2016')
         },
 
-        // Steps of one week
+/*         // Steps of one week
             step: 7 * 24 * 60 * 60 * 1000,
 
         // Two more timestamps indicate the handle starting positions.
@@ -70,8 +70,8 @@ if ($('#available-date-range')[0]) {
         // No decimals
             format: wNumb({
                 decimals: 0
-            })
-        });
+            }) */
+    });
 
     availableDateRange.noUiSlider.on('update', function( values, handle ) {
         dateValues[handle].innerHTML = formatDate(new Date(+values[handle]));
@@ -111,6 +111,7 @@ if ($('#available-date-range')[0]) {
         date.getFullYear();
     }
 }
+
 // 3. Property Area Size
 
 if ($('#property-area-range')[0]) {
