a894308340a1ae8de1a83257bfcc13bc76055413
Marcel Cruz <mcruz@consumerlaw.com>
2018-04-21 11:58:13 -0500

    aswdfqawr

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -220,13 +220,13 @@
                                     @endif  
 </div>
                                 <div class="media-body list-group__text">
-                                    <strong>{{ $listing['StreetNumber'] }} {{ $listing['StreetName'] }} {{ $listing['City'] }}</strong>
+                                    <strong>{{ $listing['StreetNumber']  ?? "" }} {{ $listing['StreetName']  ?? "" }} {{ $listing['City']  ?? "" }}</strong>
                                     <div class="listings-grid__item">
                                         
                                         <ul class="listings-grid__attrs">       
-                                            <li class="number"><i class="zmdi zmdi-money"></i>{{ $listing['Price'] }}</li>                         
-                                            <li class="number"><i class="listings-grid__icon listings-grid__icon--bed"></i> {{ $listing['Beds'] }}</li>
-                                            <li class="number"><i class="listings-grid__icon listings-grid__icon--bath"></i> {{ $listing['Baths'] }}</li>
+                                            <li class="number"><i class="zmdi zmdi-money"></i>{{ $listing['Price']  ?? "" }}</li>                         
+                                            <li class="number"><i class="listings-grid__icon listings-grid__icon--bed"></i> {{ $listing['Beds']  ?? "" }}</li>
+                                            <li class="number"><i class="listings-grid__icon listings-grid__icon--bath"></i> {{ $listing['Baths']  ?? "" }}</li>
                                         </ul>
                                     </div>
                                 </div>
