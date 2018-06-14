2dadba3212d20679cee4d960748a177e21602845
Marcel Cruz <marcelcruz85@gmail.com>
2018-06-14 06:55:20 -0500

    mobile nav

--- resources/views/components/main-header-front.blade.php
+++ resources/views/components/main-header-front.blade.php
@@ -1,17 +1,49 @@
 
-<div class="header__main" id="header_nav">
+<div class="header__main effect hidden-sm hidden-xs" id="header_nav">
     <div class="container">
         <a class="logo logo-front" href="/">
-            <img class="hidden-sm hidden-xs img-animate" src="/img/logoabove.png" style="width:320px; margin-top: 60px;" alt="">
-            <!-- <div class="logo__text">
-                <span>The Apartment</span>
-                <span>Source</span>
-            </div> -->
-            <img class="visible-sm visible-xs" src="/img/logowhite.png" style="width:320px; margin-top: 60px;" alt="">
-            <!-- <div class="logo__text">
-                <span>The Apartment</span>
-                <span>Source</span>
-            </div> -->
+            <img class="img-animate" src="/img/logoabove.png" style="width:320px; margin-top: 60px;" alt="">
+        </a>
+
+        <div class="navigation-trigger visible-xs visible-sm" data-rmd-action="block-open" data-rmd-target=".navigation">
+            <i class="zmdi zmdi-menu"></i>
+        </div>
+
+        <ul class="navigation">
+            <li class="navigation__dropdown">
+                <a href="/">Home</a>
+            </li>
+
+            <li class="navigation__dropdown">
+                <a href="/rentals/grid/updateDate-desc/search?location" {{-- class="prevent-default" --}}>Listings</a>
+            </li>
+
+
+            <li class="navigation__dropdown">
+                <a href="/submit">List</a>
+            </li>
+            
+            <li class="navigation__dropdown">
+                <a href="/forms">Forms</a>
+            </li>
+            
+            <li class="navigation__dropdown">
+                <a href="/faq">FAQ</a>
+            </li>
+
+            <li class="navigation__dropdown">
+                <a href="/contact">Contact</a>
+            </li>
+
+        </ul>
+    </div>
+</div>
+
+<div class="header__main visible-sm visible-xs" id="header_nav">
+    <div class="container">
+        <a class="logo logo-front" href="/">
+            <img  class="hidden-sm visible-xs" src="/img/logowhite.png" style="width:320px; margin-top: 60px;" alt="">
+            <img  class="hidden-xs visible-sm" src="/img/logoabove.png" style="width:320px; margin-top: 60px;" alt="">
         </a>
 
         <div class="navigation-trigger visible-xs visible-sm" data-rmd-action="block-open" data-rmd-target=".navigation">
@@ -31,15 +63,6 @@
 
             <li class="navigation__dropdown">
                 <a href="/rentals/grid/updateDate-desc/search?location" {{-- class="prevent-default" --}}>Listings</a>
-
-{{--                 <ul class="navigation__drop-menu">
-                    <li>
-                        <a href="/rentals/grid/updateDate-desc/search">Grid view</a>
-                    </li>
-                    <li>
-                        <a href="/rentals/list/updateDate-desc/search">List view</a>
-                    </li>
-                </ul> --}}
             </li>
 
 
