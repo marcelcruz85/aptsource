7f55b212eecb78db7dda7230f3e9ff12be3c30fe
marcelcruz85 <marcelcruz85@gmail.com>
2018-06-26 20:51:49 +0000

    production

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,16 @@
 <!--[if IE 9 ]><html lang="en" class="ie9"><![endif]-->
 
 <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121444877-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-121444877-1');
+</script>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
