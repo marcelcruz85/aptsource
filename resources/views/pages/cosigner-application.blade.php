e56e9c73d0209cca5dfa38a0eb0af50f18f063ae
Marcel Cruz <marcelcruz85@gmail.com>
2018-08-22 21:54:06 -0500

    apply online

--- resources/views/pages/cosigner-application.blade.php
+++ resources/views/pages/cosigner-application.blade.php
@@ -12,17 +12,23 @@
 @section ('content')
 
 <!-- content here -->
+
+<section class="section">
+        <div class="container">
 <header class="section__title section__title-alt">
     <h2>Online Application</h2>
 </header>
-<div class="row">
+<div>
     <div class="col-md-1"></div>
-    <div class="col-md-10">
-    <script type="text/javascript" src="https://apartmentsourcechicago.formstack.com/forms/js.php/rent_application"></script><noscript><a href="https://apartmentsourcechicago.formstack.com/forms/rent_application" title="Online Form">Online Form - Rent Application</a></noscript><div style="text-align:right; font-size:x-small;"><a href="http://www.formstack.com?utm_source=jsembed&utm_medium=product&utm_campaign=product+branding&fa=h,3167673" title="Powered by Formstack">Powered by Formstack</a></div>web
+    <div>
+    <script type="text/javascript" src="https://apartmentsourcechicago.formstack.com/forms/js.php/cosigner_applicacion"></script><noscript><a href="https://apartmentsourcechicago.formstack.com/forms/cosigner_applicacion" title="Online Form">Online Form - Cosigner Applicacion</a></noscript><div style="text-align:right; font-size:x-small;"><a href="http://www.formstack.com?utm_source=jsembed&utm_medium=product&utm_campaign=product+branding&fa=h,3169043" title="Powered by Formstack">Powered by Formstack</a></div>
 
     </div>
-</div>
 
+    <div class="col-md-1"></div>
+</div>
+</div>
+</section>
 
 @endsection
 
@@ -30,61 +36,7 @@
 
 <script>
     $(function () {
-        $('#tabs')
-                .tabs()
-                .addClass('ui-tabs-vertical ui-helper-clearfix');
-
-        $('#stripe-form').get(0).submit = function(){
-            console.log('forms have been submitted');
-            $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-                        // stripeToken: response.id
-                    }
-                });
-                $.ajax({
-                    type: "POST",
-                    url: '/apply',
-                    data: new FormData($("#stripe-form")[0]),
-                    // data: $("#stripe-form").serialize(),
-                    processData: false,
-                    contentType: false,
-                    cache: false,
-                    success: function (msg) {
-                        //alert(msg);
-                        if(msg == 'Approved'){
-                            $('.submit-application').show();
-                            $('#stripe-form button').hide();
-                        }
-                    }
-                });
-
-            return false;
-        }
-
-        $('#save-application').click(function (e) {
-                e.preventDefault();
-
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-                $.ajax({
-                    type: "POST",
-                    enctype: 'multipart/form-data',
-                    url: '/online-application',
-                    data: new FormData($("#online-application-form")[0]),
-                    // data: $('.contact-email').serialize(),
-                    processData: false,
-                    contentType: false,
-                    cache: false,
-                    success: function (msg) {
-                        alert('Application have been save');
-                        console.log(msg);
-                    }
-                });
-        });
+        
     });
 </script>
 <!-- Slick Carousel - Custom Alerts -->
